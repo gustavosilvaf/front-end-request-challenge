64d536ecd388fd8356ce362fa566007261bee564
gustavosilvaf <gustavofsilva@email.com>
2021-11-10 13:01:47 -0300

    removing use query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { theme } from "../styles/theme";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
